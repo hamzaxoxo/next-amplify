73f42bb06f5d662cd2f4a9ae9e4d1911bec47e6f
Muhammad Faizan <muhammad@citypt.com>
2025-01-09 01:36:00 +0500

    Add sayHello function and integrate into backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,9 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
-
+import { sayHello } from './functions/generateHaiku/resource';
 defineBackend({
   auth,
   data,
+  sayHello
 });
